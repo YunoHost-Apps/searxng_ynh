770734acb6b3b5a45cb2d6d8b3ba5bbc0a59918a
mh4ckt3mh4ckt1c4s <mh4ckt3mh4ckt1c4s@protonmail.com>
2022-11-24 21:33:12 +0100

    Remove duplicate install

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,7 +3,7 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-pkg_dependencies="python3-dev python3-babel python3-venv uwsgi uwsgi-plugin-python3 git build-essential libxslt-dev zlib1g-dev libffi-dev libssl-dev uwsgi uwsgi-plugin-python3"
+pkg_dependencies="python3-dev python3-babel python3-venv uwsgi uwsgi-plugin-python3 git build-essential libxslt-dev zlib1g-dev libffi-dev libssl-dev"
 
 #=================================================
 # UWSGI HELPERS
