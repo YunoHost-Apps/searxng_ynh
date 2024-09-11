075bc2b00f887b8cd72026fa431d753a432bd917
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-09-11 11:54:25 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -49,9 +49,9 @@ EOF
 
 # Set permissions
 myynh_set_permissions () {
-	#REMOVEME? Assuming the install dir is setup using ynh_setup_source, the proper chmod/chowns are now already applied and it shouldn't be necessary to tweak perms | chown -R $app: "$install_dir"
-	#REMOVEME? Assuming the install dir is setup using ynh_setup_source, the proper chmod/chowns are now already applied and it shouldn't be necessary to tweak perms | chmod 750 "$install_dir"
-	#REMOVEME? Assuming the install dir is setup using ynh_setup_source, the proper chmod/chowns are now already applied and it shouldn't be necessary to tweak perms | chmod -R o-rwx "$install_dir"
+	chown -R $app: "$install_dir"
+	chmod 750 "$install_dir"
+	chmod -R o-rwx "$install_dir"
 }
 
 #=================================================
