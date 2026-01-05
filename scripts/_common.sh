b45df9a817763b535f1315e8aa88f7653fa76547
ewilly <ewilly@free.fr>
2026-01-05 16:43:14 +0100

    test

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -43,7 +43,7 @@ pip install --upgrade pip
 pip install --upgrade setuptools
 pip install --upgrade wheel
 pip install --requirement "$install_dir/searxng-src/requirements.txt"
-pip install --requirement "$install_dir/searxng-src/requirements-server.txt"
+pip install --upgrade gunicorn"
 pip install --use-pep517 --no-build-isolation -e "$install_dir/searxng-src"
 EOF
 }
@@ -187,4 +187,43 @@ ynh_restore_uwsgi_service () {
 	yunohost service add "uwsgi-app@$app" \
 		--description="uWSGI service for searxng" \
 		--log "/var/log/uwsgi/$app/$app.log"
+}
+
+# Create a dedicated systemd socket config
+# usage: ynh_add_systemd_socket_config [--socket=socket] [--template=template]
+# | arg: --socket=      - Socket name (optional, `$app` by default)
+# | arg: --template=    - Name of template file (optional, this is 'systemd' by default, meaning `../conf/systemd.socket` will be used as template)
+#
+# This will use the template `../conf/<templatename>.socket`.
+#
+# See the documentation of `ynh_config_add` for a description of the template
+# format and how placeholders are replaced with actual variables.
+ynh_config_add_systemd_socket() {
+    # ============ Argument parsing =============
+    local -A args_array=([s]=socket= [t]=template=)
+    local socket
+    local template
+    ynh_handle_getopts_args "$@"
+    socket="${socket:-$app}"
+    template="${template:-systemd.socket}"
+    # ===========================================
+
+    ynh_config_add --template="$template" --destination="/etc/systemd/system/$socket.socket"
+
+    systemctl enable "$socket.socket" --quiet
+    systemctl daemon-reload
+}
+
+# Remove the dedicated systemd socket config
+#
+# usage: ynh_config_remove_systemd socket
+# | arg: socket   - Socket name (optionnal, $app by default)
+ynh_config_remove_systemd_socket() {
+    local socket="${1:-$app}"
+    if [ -e "/etc/systemd/system/$socket.socket" ]; then
+        ynh_systemctl --service="$socket" --action=stop
+        systemctl disable "$socket" --quiet
+        ynh_safe_rm "/etc/systemd/system/$socket.socket"
+        systemctl daemon-reload
+    fi
 }
